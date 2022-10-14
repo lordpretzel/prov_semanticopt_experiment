5f1cb178254f493c2387a9190f84da2ce4d924ba
lordpretzel <bglavic@iit.edu>
2022-10-14 17:05:49 -0500

    12 + 19

--- tpcq01/completeScript.sh
+++ tpcq01/completeScript.sh
@@ -1,7 +1,7 @@
 export baseName=tpcq01
 export partialPath="umflint/$baseName"
 export fullPath="umflint/$baseName/$baseName"
-export repetitions=100
+export repetitions=1
 
 echo "================================================================================
 RUN: $baseName.sh"
