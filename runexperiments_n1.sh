6511f52d220b4d1555f801a1d21ff5db78cfa04a
lordpretzel <bglavic@iit.edu>
2023-01-13 21:22:48 -0600

    new

--- runexperiments_n1.sh
+++ runexperiments_n1.sh
@@ -1,1 +1,5 @@
-python experiments.py -r 100 -q 02,03,04,05,06,07,08,09,10,11,13,14,17,19,20 -o -R results_n1
+python experiments.py -q 01,02,03,04,05,06,07,08,09,10,11,14,17,19,20 -o -O gendl
+#python experiments.py -r 100 -q 14,17,19,20 -o -R results_n1
+#python experiments.py -r 100 -q 08,09,10,11,13,14,17,19,20 -m optnoflat -o -R results_n1
+#python experiments.py -r 100 -q "14,17,19,20,01" -m optnoflat -o -R results_n1
+#python experiments.py -r 5 -q 07 -m optnoflat -o -R results_n1
